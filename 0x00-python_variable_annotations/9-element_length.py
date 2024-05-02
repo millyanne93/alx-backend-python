a575178008590c226b345912178273d63a9a3e79
millyanne93 <nmillyanne20@gmail.com>
2024-05-02 14:51:01 +0000

    9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Calculates the length of each element in the input list.
+"""
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Computes the length of a list of sequences.
+    """
+    return [(i, len(i)) for i in lst]
