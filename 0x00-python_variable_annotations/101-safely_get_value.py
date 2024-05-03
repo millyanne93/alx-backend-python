23125383b41b92cb55f9bd3d440bfb645f0e6932
millyanne93 <nmillyanne20@gmail.com>
2024-05-03 09:20:37 +0000

    fix syntax

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
+"""Given the parameters and the return values, add type
+annotations to the function
+
+Hint: look into TypeVar
+
+def safely_get_value(dct, key, default = None):
+    if key in dct:
+        return dct[key]
+    else:
+        return default
 """
-Safely retrieves a value from a dictionary based on a key.
-"""
-from typing import Mapping, Any, TypeVar, Union
 
 
-T = TypeVar('T')
+import typing
 
 
-def safely_get_value(
-    dct: Mapping,
-    key: Any,
-    default: Union[~T, None] = None
-) -> Union[Any, T]:
-    """
-    Retrieves a value from a dict using a given key.
-    """
+T = typing.TypeVar('T')
+
+
+def safely_get_value(dct: typing.Mapping, key: typing.Any, default:
+                     typing.Union[T, None] = None) -> \
+        typing.Union[typing.Any, T]:
+    """annotations of the function"""
     if key in dct:
         return dct[key]
     else:
